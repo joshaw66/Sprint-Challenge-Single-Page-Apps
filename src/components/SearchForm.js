4675810ba3c12b4cbb57bb1e660ac672ca11c13e
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-03-20 18:34:25 -0600

    Copied style to SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const CardStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 2%;
+  padding: 1%;
+  background-color: lightblue;
+`
+const TextStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+`
+
+const H1Style = styled.div`
+  color: lime;
+  font-size: 1.5rem;
+  text-shadow: 1px 1px black;
+`
 
 
 function SearchForm() {
@@ -37,14 +59,14 @@ function SearchForm() {
   
     return(
   
-    <div>
-    <img alt="character" src={character.image} /><br/>
-    <section>
-    <h1>{character.name}</h1> <br/>
-    <h2>{character.species}</h2> <br/>
-    <h2>{character.gender}</h2>
-    </section>
-    </div>
+    <CardStyle>
+    <img src = {character.image}/>
+    <TextStyle>
+      <H1Style>{character.name}</H1Style>
+      <h2>{character.species}</h2>
+      <h2>{character.gender}</h2>
+    </TextStyle>
+  </CardStyle>
   
     )
   }	
