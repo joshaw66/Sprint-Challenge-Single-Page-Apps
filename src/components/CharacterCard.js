2acd726901e9d2078d9c0212a9764228145dc066
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-03-20 18:31:55 -0600

    Added style to CharacterCard.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+
+const CardStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 2%;
+  padding: 1%;
+  background-color: lightblue;
+`
+const TextStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+`
+
+const H1Style = styled.div`
+  color: lime;
+  font-size: 1.5rem;
+  text-shadow: 1px 1px black;
+`
 
 function CharacterCard(props) {
   return (
-  <header>
+  <CardStyle>
     <img src = {props.character.image}/>
-    <div>
-      <h1>{props.character.name}</h1>
+    <TextStyle>
+      <H1Style>{props.character.name}</H1Style>
       <h2>{props.character.species}</h2>
       <h2>{props.character.gender}</h2>
-    </div>
-  </header>
+    </TextStyle>
+  </CardStyle>
   );
 }
 
