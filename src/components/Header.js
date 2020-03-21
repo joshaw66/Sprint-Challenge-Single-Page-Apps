0e5e7574a7648e78ad564bdc569b7bc6436e9dcf
Joshua Shaw <joshua@Joshuas-MacBook-Pro.local>
2020-03-20 18:06:00 -0600

    Added style to Header.js, more to come

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Navigation = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  max-width: 30%;
+  margin: auto;
+  
+  `
+const Button = styled.div`
+  padding: 10px;
+  border 3px solid lime;
+  margin: 10px;
+  border-radius: 5px;
+  background-color: lightblue;
+  &&:hover{
+    background: white;
+    color: lime;
+  }
+  `
+const StyleLink = styled(Link)`
+  text-decoration: none; 
+  color: black;
+
+`
 
 function Header() {
   return (
@@ -7,11 +33,11 @@ function Header() {
     >
       <h1>The Rick and Morty MultiVerse!</h1>
 
-    <nav>
-    <Link to='/'><button>Home</button></Link>
-    <Link to='/Search'><button>Search</button></Link>
-    <Link to='/Characters'><button>Characters</button></Link>
-    </nav>
+    <Navigation>
+    <StyleLink to='/'><Button>Home</Button></StyleLink>
+    <StyleLink to='/Search'><Button>Search</Button></StyleLink>
+    <StyleLink to='/Characters'><Button>Characters</Button></StyleLink>
+    </Navigation>
 
     </header>
   );
